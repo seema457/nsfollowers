7b5163052c3eadec4ca2c1dc6d05bb9d75dd3a60
seema457 <85431074+seema457@users.noreply.github.com>
2024-05-16 14:53:09 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='65964773461' #Enter UserID Here
+        self.__username='anujkesari090' #Enter UserName Here
+        self._session='65964773461:AB1URIQYMujvzD:10:AYf7WlSwgWpIpBhcSWrD3m1JVE3zTdk-jqb7S3k5OA' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
